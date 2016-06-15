59cc63916dd2b6ec731e7822850e274ec07604d2
kjwill555 <kjwill555@gmail.com>
2016-06-14 18:52:11 -0700

    Update HashNode.h

--- HashNode.h
+++ HashNode.h
@@ -7,26 +7,26 @@
 template <class Type>
 class HashNode {
 public:
-  std::string keyValue;
-  Type data;
-  HashNode<Type> *link;
-  int count;
-  HashNode() {
-    count = 0;
-  }
+	std::string keyValue;
+	Type data;
+	HashNode<Type> *link;
+	int count;
+	HashNode() {
+		count = 0;
+	}
 
-  void displayBucket(HashNode<Type> *bucket) {
-    HashNode<Type> *temp = bucket;
-    int tempInt = bucket->count;
-    while (temp != NULL) {
-      std::cout << temp->data << std::endl;
-      temp = temp->link;
-      if (tempInt != 1) {
-        std::cout << "Entering bucket: " << std::endl << std::endl;
-        tempInt = 1;
-      }
-    }
-  }
+	void displayBucket(HashNode<Type> *bucket) {
+		HashNode<Type> *temp = bucket;
+		int tempInt = bucket->count;
+		while (temp != NULL) {
+			std::cout << temp->data << std::endl;
+			temp = temp->link;
+			if (tempInt != 1) {
+				std::cout << "Entering bucket: " << std::endl << std::endl;
+				tempInt = 1;
+			}
+		}
+	}
 };
 
 #endif
